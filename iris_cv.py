b4f0ea595a4b1dc573e07291d8cc23ce16c91977
brenden17 <chulwook.jeon@gmail.com>
2014-07-20 08:16:00 +0900

    update iris_cv

--- iris_cv.py
+++ iris_cv.py
@@ -14,6 +14,7 @@ knn.fit(X, y)
 print('kNN score')
 print(knn.score(X, y))
 
+####################################
 # DummyClassifier
 from sklearn.dummy import DummyClassifier
 dummy = DummyClassifier(strategy='stratified',random_state=0) #stratified, most_frequent
@@ -21,6 +22,7 @@ dummy.fit(X, y)
 print('dummy score')
 print(dummy.score(X, y))
 
+####################################
 # metrics
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 knn.fit(X, y)
@@ -30,7 +32,6 @@ print(accuracy_score(knn.predict(X), y))
 import numpy as np
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
-
 
 # find k value for kNN
 # load data
@@ -61,7 +62,6 @@ def draw(x, y, title='K value for kNN'):
 draw(result, score, 'kNN')
 print("Score's mean %f" % (sum(score)/len(k_range)))
 
-
 # train_test_split
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
@@ -91,6 +91,7 @@ plt.legend(loc='best', framealpha=0.5, prop={'size':'small'})
 plt.tight_layout(pad=1)
 plt.show()
 
+####################################
 # KFold
 from sklearn.cross_validation import KFold
 folds = 5
@@ -112,6 +113,7 @@ for k in k_range:
 
 draw(k_range, score_means)
 
+####################################
 # LeaveOneOut
 from sklearn.cross_validation import LeaveOneOut
 loo = LeaveOneOut(len(y))
@@ -126,6 +128,7 @@ for train, test in loo:
 #     print('test score=%f' % (test_score))
 print('mean score %f' % (sum(scores)/150))
 
+####################################
 # cross_val_score
 from sklearn.cross_validation import cross_val_score, KFold
 kf = KFold(len(y), n_folds=5)
@@ -134,6 +137,7 @@ knn = KNeighborsClassifier(n_neighbors=5)
 scores = cross_val_score(knn, X, y, cv=kf)
 print(sum(scores)/5)
 
+####################################
 # grid_search
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import cross_val_score, KFold, ShuffleSplit
@@ -149,4 +153,46 @@ grid_search.fit(X, y)
 print(grid_search)
 print(grid_search.best_params_)
 print(grid_search.grid_scores_)
+
+
+####################################
+# try GridSearchCV with KFold
+
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_score, KFold, ShuffleSplit
+from sklearn.grid_search import GridSearchCV
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+kf = KFold(len(y), n_folds=5)
+
+## KNN
+estimators = [('pca', PCA()), ('knn', KNeighborsClassifier())]
+parameters = {'pca__n_components':(2, 3), 'knn__n_neighbors':(6,7,8,9,10,11,12,13), 'knn__weights':('uniform', 'distance')}
+
+## SVM
+# estimators = [('pca', PCA()), ('svm', SVC())]
+# parameters = {'pca__n_components':(2, 3), 'svm__C':(0.4, 1, 1.5, 2, 3)}
+
+## LogisticRegression
+# estimators = [('pca', PCA()), ('lr', LogisticRegression())]
+# parameters = {'pca__n_components':(2, 3), 'lr__penalty':( 'l1', 'l2')}
+
+pipeline = Pipeline(estimators)
+print(pipeline)
+grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, cv=kf)
+print("Performing grid search...")
+print("pipeline:", [name for name, _ in pipeline.steps])
+grid_search.fit(X, y)
+print("Best score: %0.3f" % grid_search.best_score_)
+print("Best parameters set:")
+best_parameters = grid_search.best_estimator_.get_params()
+for param_name in sorted(parameters.keys()):
+    print("\t%s: %r" % (param_name, best_parameters[param_name]))
 
